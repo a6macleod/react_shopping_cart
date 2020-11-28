1204453ad6cd3cd1560ce46d0756a8601fa36f64
Andrew MacLeod <A6macleod@gmail.com>
2020-11-28 18:01:24 -0500

    Add local state to ItemView for onChange

--- src/components/ItemView.js
+++ src/components/ItemView.js
@@ -1,11 +1,19 @@
+import React, { useState } from "react";
+
 const ItemView = (props) => {
+  const [itemAmount, setItemAmount] = useState(1);
+
+  const handleChange = (e) => {
+    setItemAmount(e.target.value);
+  };
+
   return (
-    <div>
+    <div className="itemView">
       <h2>{props.item.id}</h2>
       <h4>{props.item.cost}</h4>
       <img src={props.item.img} alt="" />
-      <input type="number" />
-      <button onClick={() => props.addItem("one", 1)}>add to cart</button>
+      <input type="number" value={itemAmount} onChange={handleChange} />
+      <button onClick={() => props.addItem(props.item)}>add to cart</button>
     </div>
   );
 };
