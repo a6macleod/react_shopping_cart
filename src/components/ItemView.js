c9a79549196fc9ca47b7d022d44beb08406d8fab
Andrew MacLeod <A6macleod@gmail.com>
2020-12-06 21:35:07 -0500

    Style ItemView

--- src/components/ItemView.js
+++ src/components/ItemView.js
@@ -1,12 +1,18 @@
 import Quantity from "./Quantity";
+import "./ItemView.css";
 
 const ItemView = (props) => {
   return (
     <div className="itemView">
-      <h2>{props.item.name}</h2>
-      <h4>{props.item.cost}</h4>
-      <img src={props.item.img} alt="" />
-      <Quantity addItem={props.addItem} item={props.item} />
+      <div className="imgContainer">
+        <img src={props.item.img} alt={props.item.brief} />
+      </div>
+      <div className="itemInfo">
+        <h2>{props.item.name}</h2>
+        <h4>${props.item.cost}</h4>
+        <Quantity addItem={props.addItem} item={props.item} />
+        <p>{props.item.description}</p>
+      </div>
     </div>
   );
 };
--- src/components/Quantity.js
+++ src/components/Quantity.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./Quantity.css";
 
 const Quantity = (props) => {
   const [itemAmount, setItemAmount] = useState(1);
