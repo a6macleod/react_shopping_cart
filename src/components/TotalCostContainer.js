21ebcba5829148636c0298c3f163133648c56ecd
Andrew MacLeod <A6macleod@gmail.com>
2022-04-04 21:48:07 -0400

    fix indent issues on TotalCostContainer

--- src/components/TotalCostContainer.js
+++ src/components/TotalCostContainer.js
@@ -1,17 +1,19 @@
 const TotalCostContainer = ({ checkoutInfo }) => {
   return (
     <div className="totalsContainer">
-    <div className="totals">
-      <p className="subtotal">
-        SUBTOTAL: {`$${checkoutInfo.subtotalPrice}`}
-      </p>
-      <p className="tax">TAX: {`$${checkoutInfo.taxPrice}`}</p>
-      <p className="shipping">
-        SHIPPING: {`$${checkoutInfo.shippingPrice}`}
-      </p>
-      <h4 className="total">TOTAL: {`$${checkoutInfo.totalPrice}`}</h4>
-      <button className="checkout">CHECKOUT</button>
-    </div>
+      <div className="totals">
+        <p className="subtotal">
+          SUBTOTAL: {`$${checkoutInfo.subtotalPrice}`}
+        </p>
+        <p className="tax">
+          TAX: {`$${checkoutInfo.taxPrice}`}
+        </p>
+        <p className="shipping">
+          SHIPPING: {`$${checkoutInfo.shippingPrice}`}
+        </p>
+        <h4 className="total">TOTAL: {`$${checkoutInfo.totalPrice}`}</h4>
+        <button className="checkout">CHECKOUT</button>
+      </div>
   </div>
   );
 };
